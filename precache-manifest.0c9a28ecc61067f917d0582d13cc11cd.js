5d7dd3c1a514f2a714b6f9a44bbe4864bc8a8be5
Volodymyr <vovakrivickiy34@gmail.com>
2019-06-10 19:23:55 +0200

    Updates

---
+++ precache-manifest.0c9a28ecc61067f917d0582d13cc11cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3e8cecdd6d24868509d8097350ac6f8",
+    "url": "/phonesApp-react/index.html"
+  },
+  {
+    "revision": "ebf43220ea0231f233a3",
+    "url": "/phonesApp-react/static/css/main.bfd8e9de.chunk.css"
+  },
+  {
+    "revision": "ad4386e8906573f84443",
+    "url": "/phonesApp-react/static/js/2.31625c1d.chunk.js"
+  },
+  {
+    "revision": "ebf43220ea0231f233a3",
+    "url": "/phonesApp-react/static/js/main.e168ebaf.chunk.js"
+  },
+  {
+    "revision": "1eeea0ad9e276e858c51",
+    "url": "/phonesApp-react/static/js/runtime~main.0c6c941e.js"
+  }
+]);
